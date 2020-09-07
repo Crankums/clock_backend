aa593b954858cf8562eebf79b1d1d1fcf543fd0a
Craig_Donavin <crdonavin@gmail.com>
2020-09-06 19:51:55 -0500

    add employee seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,21 @@
 
 ##Write Employee Seed File!
 
- employees = Employee.create({ name: "Frank Johnson",
+ employees = Employee.create([{ name: "Frank Johnson",
         employee_id: 0001,
         password: "password",
+        email: "fjohnson@clocko.com",
         phone_number: "555-123-5678",
         job_id: 0100},
         { name: "Jenny O'Malley",
         employee_id: 0002,
         password: "password",
+        email: "jomalley@clocko.com",
         phone_number: "246-810-1214",
         job_id: 0200},
         { name: "Alice Fujita",
         employee_id: 0003,
         password: "password",
+        email: "afujita@clocko.com",
         phone_number: "123-567-8910",
-        job_id: 0300})
+        job_id: 0300}])
