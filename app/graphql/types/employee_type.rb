8ac5411dc25b3fdd82ba7310489b26f12a709cf0
Craig_Donavin <crdonavin@gmail.com>
2020-09-09 16:44:31 -0500

    install graphQL, create Employee object

---
+++ app/graphql/types/employee_type.rb
@@ -0,0 +1,13 @@
+module Types
+  class EmployeeType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: true
+    field :employee_id, Integer, null: true
+    field :password_digest, String, null: true
+    field :email, String, null: true
+    field :phone_number, String, null: true
+    field :job_id, Integer, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
