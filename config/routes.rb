c1388e0c415ebe9427b67ef27c8fdf83fed0ed2d
Craig_Donavin <crdonavin@gmail.com>
2020-09-10 14:23:17 -0500

    fix typo in graphiql route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at "/graphiql", graphiql_path: "graphql#execute"
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphiql_path: "graphql#execute"
   end
   post "/graphql", to: "graphql#execute"
   namespace :api do
