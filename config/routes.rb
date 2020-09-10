bc0166acf2f255f406947142e00f31507afb25a9
Craig_Donavin <crdonavin@gmail.com>
2020-09-10 13:28:46 -0500

    add graphiql-rails endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at "/graphiql", graphiql_path: "graphql#execute"
+  end
   post "/graphql", to: "graphql#execute"
   namespace :api do
     namespace :v1 do
