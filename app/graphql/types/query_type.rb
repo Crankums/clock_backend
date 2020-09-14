599eea87a4bb61de53c105b7d4af05bc81e23304
Craig_Donavin <crdonavin@gmail.com>
2020-09-13 21:04:00 -0500

    add employee query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,18 @@
 module Types
   class QueryType < Types::BaseObject
     
-  end
+    field :employees, [Types::EmployeeType], null: false
+
+    def employees
+      Employee.all
+    end
+    
+    field :employee, Types::EmployeeType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def employee(id:)
+      Employee.find(id)
+
+
 end
