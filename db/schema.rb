8ec77c8d663a593059292e4046305f8fc8ef7c7f
Craig_Donavin <crdonavin@gmail.com>
2020-09-13 20:39:17 -0500

    migrated: add todays_date to punches table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_033633) do
+ActiveRecord::Schema.define(version: 2020_09_14_013456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_09_02_033633) do
     t.integer "employee_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "todays_date"
   end
 
   create_table "timecards", force: :cascade do |t|
