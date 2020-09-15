56537d805ea9dc7aaee4a38270a71e995035c434
Craig_Donavin <crdonavin@gmail.com>
2020-09-14 21:12:55 -0500

    comments out field :create_punch until create_employee is working correctly

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
    field :create_employee, mutation: Mutations::CreateEmployee
-   field :create_punch, mutation: Mutations::CreatePunch
+  #  field :create_punch, mutation: Mutations::CreatePunch
   end
 end
