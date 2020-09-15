e3809b534a0e19c8d1d2cf9a31537966d03894b8
Craig_Donavin <crdonavin@gmail.com>
2020-09-14 20:35:36 -0500

    add fields create_employee and _punch to mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+   field :create_employee, mutation: Mutations::CreateEmployee
+   field :create_punch, mutation: Mutations::CreatePunch
   end
 end
