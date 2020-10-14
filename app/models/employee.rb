24ec9a6fd335acab8e0cc174d529454ac05d4b89
Craig_Donavin <crdonavin@gmail.com>
2020-10-13 22:09:43 -0500

    add class method Employee#email_builder

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,10 @@ class Employee < ApplicationRecord
     has_secure_password
     has_many :punches
     has_one :timecard
+
+    def email_builder(string)
+        name_arr = string.split(' ')
+        lead = name_arr[0][0]+name_arr[1]
+        lead.downcase+"@clocko.com"
+    end 
 end
