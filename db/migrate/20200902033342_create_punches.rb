fd825c16c38dade09204ca0ad4e058ba2b01fa19
Craig_Donavin <crdonavin@gmail.com>
2020-12-15 23:20:42 -0600

    change punch migration

--- db/migrate/20200902033342_create_punches.rb
+++ db/migrate/20200902033342_create_punches.rb
@@ -1,10 +1,11 @@
 class CreatePunches < ActiveRecord::Migration[6.0]
   def change
     create_table :punches do |t|
-      t.datetime :clock_in
-      t.datetime :clock_out
-      t.datetime :break_start
+      t.time :punch_time
+      t.date :todays_date
+      t.string :punch_type
       t.integer :employee_id
+      t.integer :timecard_id
 
       t.timestamps
     end
--- db/migrate/20200914013456_add_todays_date_to_punch.rb
+++
@@ -1,5 +0,0 @@
-class AddTodaysDateToPunch < ActiveRecord::Migration[6.0]
-  def change
-    add_column :punches, :todays_date, :datetime
-  end
-end
