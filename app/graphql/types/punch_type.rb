6f6f3df6c19b6163dc80f0ef4b8007bd535a6f95
Craig_Donavin <crdonavin@gmail.com>
2020-12-18 22:15:36 -0600

    overwrite existing punch type

--- app/graphql/types/punch_type.rb
+++ app/graphql/types/punch_type.rb
@@ -1,16 +1,12 @@
 module Types
   class PunchType < Types::BaseObject
     field :id, ID, null: false
-    field :clock_in, GraphQL::Types::ISO8601DateTime, null: true
-    field :clock_out, GraphQL::Types::ISO8601DateTime, null: true
-    field :break_start, GraphQL::Types::ISO8601DateTime, null: true
+    field :punch_time, Types::TimeType, null: true
+    field :todays_date, GraphQL::Types::ISO8601Date, null: true
+    field :punch_type, String, null: true
     field :employee_id, Integer, null: true
+    field :timecard_id, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :todays_date, GraphQL::Types::ISO8601DateTime, null: true
-
-    def last_punch
-      object.first
-    end
   end
 end
