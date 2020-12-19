f0098aa11e52450de4ee86dffa0c26a8d9e418db
Craig_Donavin <crdonavin@gmail.com>
2020-12-18 22:43:56 -0600

    add #create_timecard method, attributes to punch create

--- app/graphql/mutations/create_punch.rb
+++ app/graphql/mutations/create_punch.rb
@@ -1,13 +1,29 @@
 class Mutations::CreatePunch < Mutations::BaseMutation
     argument :todays_date, DateTime, required: true
     argument :employee_id, Integer, required: true
+    argument :punch_time, Time, required: true
+    argument :punch_type, String, required: true
 
     field :punch, Types::PunchType, null: false
     field :errors, [String], null: false
 
     def resolve(todays_date:, employee_id:)
         employee = Employee.find(id: employee_id)
-        punch = employee.punch.build(todays_date: todays_date, employee_id: employee_id)
+        if !employee.timecard
+            {
+                employee.create_timecard(
+                    week_start: Date.today.beginning_of_week(:monday),
+                    week_end: Date.today+4
+                )
+            }
+        punch = employee.punch.build(
+            todays_date: todays_date, 
+            employee_id: employee_id, 
+            punch_type: punch_type, 
+            punch_time: punch_time
+            timecard_id: employee.timecard.id
+        )
+        
         if punch.save 
             {
                 punch: punch,
