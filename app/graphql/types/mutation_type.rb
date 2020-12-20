b1a461cd44a5ed979915e4e6c66bd1033f768ee9
Craig_Donavin <crdonavin@gmail.com>
2020-12-20 00:05:11 -0600

    add :create_timecard field

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
-   field :create_employee, mutation: Mutations::CreateEmployee
-  #  field :create_punch, mutation: Mutations::CreatePunch
+    field :create_timecard, mutation: Mutations::CreateTimecard
+    field :create_employee, mutation: Mutations::CreateEmployee
+    field :create_punch, mutation: Mutations::CreatePunch
   end
 end
