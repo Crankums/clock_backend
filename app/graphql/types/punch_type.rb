25832dbd0951b64b62969b0292806ba6387515f1
Craig_Donavin <crdonavin@gmail.com>
2020-12-19 22:52:27 -0600

    fix TimeType -> DateTimeType in PunchType, add timecard to EmployeeType

--- app/graphql/types/punch_type.rb
+++ app/graphql/types/punch_type.rb
@@ -1,12 +1,17 @@
 module Types
   class PunchType < Types::BaseObject
     field :id, ID, null: false
-    field :punch_time, Types::TimeType, null: true
+    field :punch_time, GraphQL::Types::ISO8601DateTime, null: true
     field :todays_date, GraphQL::Types::ISO8601Date, null: true
     field :punch_type, String, null: true
     field :employee_id, Integer, null: true
     field :timecard_id, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def last_punch
+      object.last
+    end
+
   end
 end
