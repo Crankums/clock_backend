1b5885e1bea744b7b6f80f0ac60cbc224e2621d9
Craig_Donavin <crdonavin@gmail.com>
2020-12-21 21:27:11 -0600

    Moved #day scope method from timecard to punch

--- app/models/punch.rb
+++ app/models/punch.rb
@@ -2,4 +2,6 @@ class Punch < ApplicationRecord
     belongs_to :employee
     belongs_to :timecard
 
+    scope :day, ->(date) { where("todays_date = ?", date)}
+
 end
--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -2,13 +2,4 @@ class Timecard < ApplicationRecord
     belongs_to :employee
     has_many :punches
 
-    scope :day, ->(date) { where("todays_date = ?", date)}
-
-
-    # valid punch_types: clock_in, clock_out, break_start
-    # Rules: clock_out > clock_in,
-    # Break: break_start > clock_in && must be followed by a clock_in
-    # clock_in must be followed by clock_out
-    # punches with matching "todays_date" will be collected into days, which will then make up the rest of the calendar 
-
 end
