b93d8887a53c445391898e6008f747fa78e30e2c
Craig_Donavin <crdonavin@gmail.com>
2020-12-27 13:55:29 -0600

    create punch checker methods

--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -2,8 +2,16 @@ class Timecard < ApplicationRecord
     belongs_to :employee
     has_many :punches
    
+    def day_valid(day)
+        if day.include(type: "clockin") && day.include(type:"clockout")
+            &&
+        if clockout > clockin || if clockout > clockin && meal_start
+    end
+
+
     def day_checker(todays_date)
         day = Punch.day(todays_date)
+
     end
 
 end
