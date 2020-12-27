ca8b7cab3bd00dd0809021e8818ff4519ab29acd
Craig_Donavin <crdonavin@gmail.com>
2020-12-26 23:42:39 -0600

    checkable module pseudocode

--- app/models/concerns/checkable.rb
+++ app/models/concerns/checkable.rb
@@ -1,5 +1,12 @@
 module Checkable
 
+    # day scope method where(date){'todays_date = ?', date}
+
+    # Timecard checking rules:
+    # -clockin > clockout
+    # -break start > clockin && break start < clockout
+    # -all "days" must include clockin && clockout 
+    #     -otherwise day.status == 'invalid'
     
     
 end
